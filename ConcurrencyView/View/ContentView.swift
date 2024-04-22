3e34cffb838e8cd40dda760c233326086f878574
Aula Faridah <aulafaridah@gmail.com>
2024-04-22 10:18:46 +0700

    Synchronous and Asynchronous Method

--- ConcurrencyView/View/ContentView.swift
+++ ConcurrencyView/View/ContentView.swift
@@ -17,21 +17,32 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             ScrollView{
-                LazyVGrid(columns: columns, spacing: 20) {
+                LazyVGrid(columns: columns, spacing: 24) {
                     ForEach(symbolVM.symbols) { item in
                         Image(systemName: item.name)
-                            .padding()
+//                            .padding()
                             .foregroundStyle(.white)
                             .font(.largeTitle)
                             .frame(width: 100, height: 100)
                             .background(
-                                LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.cyan]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                                LinearGradient(gradient: Gradient(colors: [Color.cyan, Color.indigo]), startPoint: .topLeading, endPoint: .bottomTrailing)
                             )
                             .clipShape(RoundedRectangle(cornerRadius: 18))
                     }
                 }
             }
             .navigationTitle("Symbols")
+            .toolbar {
+                Button{
+                    Task{
+//                        symbolVM.downloadImageAndBlockUI()
+                       await symbolVM.downloadImageWithoutBlockingUI()
+                    }
+                } label: {
+                    Image(systemName: "square.and.arrow.down")
+                }
+                .tint(.black)
+            }
         }
     }
 }
--- ConcurrencyView/ViewModel/SymbolVM.swift
+++ ConcurrencyView/ViewModel/SymbolVM.swift
@@ -9,4 +9,36 @@ import Foundation
 
 class SymbolVM: ObservableObject {
     @Published var symbols: [Symbol] = Symbol.dummyData
+    
+    
+    // MARK: -ASYNCHRONOUS
+    func downloadImageWithoutBlockingUI() async {
+        let urlString = "https://res.cloudinary.com/dom97btrg/image/upload/v1713407655/samples/coffee.jpg?uuid=\(UUID().uuidString)"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            print(data)
+            print(url)
+        } catch {
+            print("Error downloading image: \(error)")
+        }
+    }
+    
+    // MARK: -SYNCHRONOUS
+    func downloadImageAndBlockUI() {
+        let urlString = "https://res.cloudinary.com/dom97btrg/image/upload/v1713407655/samples/coffee.jpg?uuid=\(UUID().uuidString)"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        DispatchQueue.global().sync {
+            Thread.sleep(forTimeInterval: 2)
+            let output = try? Data(contentsOf: url)
+            
+            print(output!)
+            print(url)
+        }
+    }
 }
